4a17afb305dd6f3ebc62e1ac3cfd45b80b2aff5c
Ian <iangerstel@gmail.com>
2014-07-09 22:19:37 -0700

    implementing test for fbimg model.

--- app/models/fb_img.rb
+++ app/models/fb_img.rb
@@ -1,2 +1,35 @@
 class FbImg < ActiveRecord::Base
+
+  def self.get_oauth_token
+    oauth = Koala::Facebook::OAuth.new(ENV['MYDOG_ID'], ENV['MYDOG_SECRET'], nil)
+    return oauth.get_app_access_token
+  end
+
+  def self.get_album_ids(token)
+    graph = Koala::Facebook::API.new(token)
+    dog_albums = graph.get_connections(ENV['BARKLEY_ID'],'albums')
+    album_ids = []
+    dog_albums.each {|d| album_ids << d['id'] unless d['name'] == 'Untitled Album'}
+    return album_ids
+  end
+
+  def self.get_photo_urls(photos)
+    photo_urls = []
+    photos.each {|photo| photo_urls << photo['images'].first['source'] }
+    return photo_urls
+  end
+
+  def self.valid_fb_url(photo_url)
+    http = photo_url.include?('http')
+    fbcdn = photo_url.include?('fbcdn')
+    last_three = /([a-zA-Z]{3})/.match(photo_url[-3..-1])
+    ext = (last_three.length == 3)
+    return (http && fbcdn && ext)
+  end
+
+  def self.valid_image(photo_url)
+    ext = photo_url.split('.').last
+    return ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'tif', 'tiff'].include?(ext)
+  end
+
 end
--- test/models/fb_img_test.rb
+++ test/models/fb_img_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
-class FbImgTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class FbImgTest do
+
+  it "should get oAuth token" do
+    token = FbImg.get_oauth_token
+    token.class.should == String
+  end
+
+  it "should get album IDs"
+    token = FbImg.get_oauth_token
+    FbImg.get_album_ids(token)
+    album_ids.count.should == 7
+  end
+
+  it "should get a photo URL for an album" do
+    token = FbImg.get_oauth_token
+    albums_ids = FbImg.get_album_ids(token)
+    photos = graph.get_connections(album_ids.first, 'photos')
+    photo_url = photos.first['images'].first['source']
+    photo_url.include?('fbcdn.net').should == true
+  end
+
+  it "should get an array of photo URLs" do
+    token = FbImg.get_oauth_token
+    albums_ids = FbImg.get_album_ids(token)
+    photos = graph.get_connections(album_ids.first, 'photos')
+    photo_urls = FbImg.get_photo_urls(photos)
+    photo_urls.class.should == Array && photo_urls.last.include?('fbcdn.net').should == true
+  end
+
+  it "should check a photo URL for http, fbcdn.net, and an extension" do
+    good_url = "https://scontent-b.xx.fbcdn.net/hphotos-xpa1/t1.0-9/10540843_934068386608655_3563804495790302463_n.jpg"
+    valid_url = FbImg.valid_fb_url(good_url)
+    valid_url.should == true
+  end
+
+  it "should check photo URL for http/fbcdn.net/extension and discard bad url" do
+    bad_url = "https://scontent-b.xx.fbcdn.net/"
+    valid_url = FbImg.valid_fb_url(bad_url)
+    valid_url.should == false
+  end
+
+  it "should discard images with bad extensions" do
+    photo_url = 'www.fakeurl.com/not_image.mp3'
+    valid_image = FbImg.valid_image(photo_url)
+    valid_image.should == false
+  end
+
+  it "should allow images with proper extensions" do
+    photo_url = 'www.fakeurl.com/image.jpg'
+    valid_image = FbImg.valid_image(photo_url)
+    valid_image.should == true
+  end
+
+
+
 end
